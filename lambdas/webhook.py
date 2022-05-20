bcdf8fe1985738c30c651df1b0f0122e671657f3
Helen Bailey <hbailey@mit.edu>
2022-05-20 16:49:24 -0400

    Add POST request handler and signature validation
    Why these changes are being introduced:
    Handling webhook POST requests from Alma is the primary needed
    functionality for this lambda. But before any POST request is
    processed, we need to ensure it came from Alma by validating the
    signature.
    
    How this addresses that need:
    * Adds POST request handler to the webhook lambda function with a
      validate_signature function, returning a "no action taken" success
      response if the signature is valid. This will become the default
      response for JOB_END POST requests that don't trigger any action
      (most of them).
    * For an invalid signature, logs a warning message for local
      investigation and returns a 401 response to Alma.
    * Adds more detailed logging to ensure that each request received
      generates a useful result log (lambda functions don't include the
      return value in Cloudwatch logs).
    * Adds unit tests for all new functionality.
    * Updates README to include new env variable and local POST request
      testing instructions.
    
    Side effects of this change:
    * All POST requests coming from Alma will now receive responses.
    * The terraform for this app will need to include the new env variable,
      which must match the configured challenge secret in the relevant
      Alma instance for the environment.
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/ENSY-67

--- lambdas/webhook.py
+++ lambdas/webhook.py
@@ -1,14 +1,16 @@
+import base64
+import hmac
 import json
 import logging
 import os
 
 import sentry_sdk
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 def lambda_handler(event: dict, context: object) -> dict[str, object]:
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.DEBUG)
-
     env = os.environ["WORKSPACE"]
     if sentry_dsn := os.getenv("SENTRY_DSN"):
         sentry_sdk.init(sentry_dsn, environment=env)
@@ -23,13 +25,20 @@ def lambda_handler(event: dict, context: object) -> dict[str, object]:
         "isBase64Encoded": False,
     }
 
-    request_type = event["requestContext"]["http"]["method"]
-    if request_type == "GET":
+    request_method = event["requestContext"]["http"]["method"]
+    if request_method == "GET":
         result = handle_get_request(event)
+    elif request_method == "POST":
+        result = handle_post_request(event)
     else:
+        logger.warning(
+            "Received invalid HTTP request method %s, returning 405 error response.",
+            request_method,
+        )
         result = {
             "statusCode": 405,
-            "body": "HTTP method not allowed. Only GET requests are supported.",
+            "body": f"HTTP method {request_method} not allowed. Supported methods: "
+            "GET, POST.",
         }
 
     return base_response | result
@@ -39,11 +48,48 @@ def handle_get_request(event: dict) -> dict[str, object]:
     try:
         challenge_secret = event["queryStringParameters"]["challenge"]
     except KeyError:
+        logger.warning(
+            "Received GET request without 'challenge' query parameter, returning "
+            "400 error response."
+        )
         return {
             "statusCode": 400,
-            "body": "Malformed request, 'challenge' query parameter required",
+            "body": "Malformed request, 'challenge' query parameter required.",
         }
+    logger.info("GET request received, returning 200 success response.")
     return {
         "statusCode": 200,
         "body": challenge_secret,
     }
+
+
+def handle_post_request(event: dict) -> dict[str, object]:
+    if not valid_signature(event):
+        logger.warning(
+            "Invalid signature in POST request, returning 401 error response."
+        )
+        return {
+            "statusCode": 401,
+            "body": "Unable to validate signature. Has the webhook challenge secret "
+            "changed?",
+        }
+    logger.info(
+        "POST request received and validated, no action triggered. Returning 200 "
+        "success response."
+    )
+    return {
+        "statusCode": 200,
+        "body": "Webhook POST request received and validated, no action taken.",
+    }
+
+
+def valid_signature(event: dict) -> bool:
+    secret = os.environ["ALMA_CHALLENGE_SECRET"]
+    message = json.dumps(event["body"])
+    try:
+        request_signature = event["headers"]["x-exl-signature"]
+    except KeyError:
+        return False
+    message_hash = hmac.digest(secret.encode(), msg=message.encode(), digest="sha256")
+    expected_signature = base64.b64encode(message_hash).decode()
+    return request_signature == expected_signature
